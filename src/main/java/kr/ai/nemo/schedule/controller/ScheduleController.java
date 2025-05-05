7b5a4aee280bfbf9cd884cc3bcdfb4a141ca515d
kdaehun00 <kdaehun00@naver.com>
2025-05-05 18:41:14 +0900

    feat: 일정 삭제(취소) Service logic 추가 (#37)

--- src/main/java/kr/ai/nemo/schedule/controller/ScheduleController.java
+++ src/main/java/kr/ai/nemo/schedule/controller/ScheduleController.java
@@ -9,6 +9,8 @@ import kr.ai.nemo.schedule.service.ScheduleCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +28,12 @@ public class ScheduleController {
     ScheduleCreateResponse response = scheduleCommandService.createSchedule(userId, request);
     URI location = UriGenerator.scheduleDetail(response.group().groupId());
     return ResponseEntity.created(location).body(ApiResponse.created(response));
+  }
+
+  @DeleteMapping("/{scheduleId}")
+  public ResponseEntity<ApiResponse<Void>> deleteSchedule(@PathVariable Long scheduleId) {
+    Long userId = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getName());
+    scheduleCommandService.deleteSchedule(userId, scheduleId);
+    return ResponseEntity.noContent().build();
   }
 }
