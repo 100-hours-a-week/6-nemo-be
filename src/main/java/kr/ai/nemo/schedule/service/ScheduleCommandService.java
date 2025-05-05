a8146b242d52bef102b4e06ef0a4b012c96969a6
kdaehun00 <kdaehun00@naver.com>
2025-05-05 18:56:03 +0900

    feat: 일정 삭제(취소) API 개발 (#37)

--- src/main/java/kr/ai/nemo/schedule/service/ScheduleCommandService.java
+++ src/main/java/kr/ai/nemo/schedule/service/ScheduleCommandService.java
@@ -1,5 +1,8 @@
 package kr.ai.nemo.schedule.service;
 
+import java.time.LocalDateTime;
+import kr.ai.nemo.common.exception.CustomException;
+import kr.ai.nemo.common.exception.ResponseCode;
 import kr.ai.nemo.group.participants.service.GroupParticipantsService;
 import kr.ai.nemo.group.service.GroupQueryService;
 import kr.ai.nemo.schedule.domain.Schedule;
@@ -10,6 +13,7 @@ import kr.ai.nemo.schedule.repository.ScheduleRepository;
 import kr.ai.nemo.user.service.UserQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -35,5 +39,21 @@ public class ScheduleCommandService {
     scheduleRepository.save(schedule);
 
     return ScheduleCreateResponse.from(schedule);
+  }
+
+  @Transactional
+  public void deleteSchedule(Long userId, Long scheduleId) {
+    Schedule schedule = scheduleRepository.findById(scheduleId)
+        .orElseThrow(() -> new CustomException(ResponseCode.SCHEDULE_NOT_FOUND));
+
+    if (schedule.getStartAt().isBefore(LocalDateTime.now())) {
+      throw new CustomException(ResponseCode.SCHEDULE_ALREADY_ENDED);
+    }
+
+    if (!schedule.getOwner().getId().equals(userId)) {
+      throw new CustomException(ResponseCode.SCHEDULE_DELETE_FORBIDDEN);
+    }
+
+    schedule.cancel();
   }
 }
