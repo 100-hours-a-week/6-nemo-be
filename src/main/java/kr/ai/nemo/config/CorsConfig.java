706d16918f28a7afbb369a0a2c56b76e42c7cb50
kdaehun00 <kdaehun00@naver.com>
2025-05-07 16:35:58 +0900

    도메인 CORS 허용

--- src/main/java/kr/ai/nemo/config/CorsConfig.java
+++ src/main/java/kr/ai/nemo/config/CorsConfig.java
@@ -13,11 +13,10 @@ public class CorsConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8000"));
+    configuration.setAllowedOrigins(List.of("https://dev.nemo.ai.kr", "http://localhost:3000", "http://localhost:8000"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
-
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
