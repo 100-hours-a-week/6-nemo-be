8b3703ca5ab6157979fc6615929193e833d4c43c
kdaehun00 <kdaehun00@naver.com>
2025-05-12 14:39:02 +0900

    feat: refresh token 전달 방식 변경 (cookie-HttpOnly)  (#74)

--- src/main/java/kr/ai/nemo/auth/controller/OauthController.java
+++ src/main/java/kr/ai/nemo/auth/controller/OauthController.java
@@ -1,5 +1,6 @@
 package kr.ai.nemo.auth.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import kr.ai.nemo.auth.dto.TokenRefreshResponse;
 import kr.ai.nemo.auth.exception.OAuthErrorCode;
 import kr.ai.nemo.auth.exception.OAuthException;
@@ -8,6 +9,7 @@ import kr.ai.nemo.common.exception.ApiResponse;
 import kr.ai.nemo.user.dto.UserLoginResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -24,7 +26,8 @@ public class OauthController {
   public ResponseEntity<ApiResponse<UserLoginResponse>> kakaoLogin(
       @RequestParam(value = "code", required = false) String code,
       @RequestParam(value = "error", required = false) String error,
-      @RequestParam(value = "error_description", required = false) String errorDescription) {
+      @RequestParam(value = "error_description", required = false) String errorDescription,
+      HttpServletResponse response) {
 
     if (error != null) {
       throw new OAuthException(OAuthErrorCode.KAKAO_AUTH_ERROR);
@@ -34,15 +37,13 @@ public class OauthController {
       throw new OAuthException(OAuthErrorCode.CODE_MISSING);
     }
 
-    UserLoginResponse response = oauthService.loginWithKakao(code);
-    return ResponseEntity.ok(ApiResponse.success(response));
+    return ResponseEntity.ok(ApiResponse.success(oauthService.loginWithKakao(code, response)));
   }
 
   @PostMapping("/api/v1/auth/token/refresh")
   public ResponseEntity<ApiResponse<TokenRefreshResponse>> tokenRefresh(
-      @RequestHeader("Authorization") String refreshToken
+      @CookieValue(name = "refresh_token", required = true) String refreshToken
   ) {
-    TokenRefreshResponse response = oauthService.reissueAccessToken(refreshToken);
-    return ResponseEntity.ok(ApiResponse.success(response));
+    return ResponseEntity.ok(ApiResponse.success(oauthService.reissueAccessToken(refreshToken)));
   }
 }
