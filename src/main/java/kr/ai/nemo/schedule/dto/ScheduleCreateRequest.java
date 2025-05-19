3acb4f7b13f44a96e248e946c7b6a7806a5e61a2
kdaehun00 <kdaehun00@naver.com>
2025-05-19 15:13:54 +0900

    fix: request 시간 타입 변경 (#99)

--- src/main/java/kr/ai/nemo/schedule/dto/ScheduleCreateRequest.java
+++ src/main/java/kr/ai/nemo/schedule/dto/ScheduleCreateRequest.java
@@ -13,7 +13,7 @@ public record ScheduleCreateRequest(
     String addressDetail,
 
     @NotNull
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
     LocalDateTime startAt
 
 ) {
