241391021cf077bbd2b6bf70552842906252750a
kdaehun00 <kdaehun00@naver.com>
2025-05-30 22:51:30 +0900

    refactor: 모임 상세 조회 DB 쿼리 개선

--- src/main/java/kr/ai/nemo/domain/group/repository/GroupTagRepository.java
+++ src/main/java/kr/ai/nemo/domain/group/repository/GroupTagRepository.java
@@ -2,11 +2,13 @@ package kr.ai.nemo.domain.group.repository;
 
 import java.util.List;
 import kr.ai.nemo.domain.group.domain.GroupTag;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GroupTagRepository extends JpaRepository<GroupTag, Long> {
 
+  @EntityGraph(attributePaths = {"tag"})
   List<GroupTag> findByGroupId(Long groupId);
 }
