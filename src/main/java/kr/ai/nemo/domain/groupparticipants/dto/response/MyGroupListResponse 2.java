2ecf95a5227b247ddc906b94ba2ddcdb7980694f
kdaehun00 <kdaehun00@naver.com>
2025-06-13 19:47:20 +0900

    hotfix: 배포 오류 해결

--- src/main/java/kr/ai/nemo/domain/groupparticipants/dto/response/MyGroupListResponse 2.java
+++
@@ -1,9 +0,0 @@
-package kr.ai.nemo.domain.groupparticipants.dto.response;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.List;
-
-@Schema(name = "나의 모임 리스트 목록", description = "나의 모임 리스트 응답 DTO")
-public record MyGroupListResponse(
-    List<MyGroupDto> groups
-) {}
