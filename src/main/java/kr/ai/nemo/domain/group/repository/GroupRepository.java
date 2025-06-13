9f46ac25bab67873ff98a8e87a392673f9d5e2b9
kdaehun00 <kdaehun00@naver.com>
2025-06-13 17:11:44 +0900

    hotfix: v2 배포 에러 해결 및 개선 작업 (#147)
    * hotfix: 일정 참석 거절 시 처리 안 되는 오류 해결 (#145)
    
    * feat: 모임 list 출력 정렬값 추가
    
    * feat: 모임 updatedAt 자동 업데이트 삭제, setter 추가
    
    * feat: 모임 정렬 변경

--- src/main/java/kr/ai/nemo/domain/group/repository/GroupRepository.java
+++ src/main/java/kr/ai/nemo/domain/group/repository/GroupRepository.java
@@ -2,7 +2,6 @@ package kr.ai.nemo.domain.group.repository;
 
 import kr.ai.nemo.domain.group.domain.Group;
 import kr.ai.nemo.domain.group.domain.enums.GroupStatus;
-import kr.ai.nemo.domain.groupparticipants.domain.enums.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -23,13 +22,31 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
         g.summary LIKE %:keyword% OR
         t.name LIKE %:keyword%
     )
+    ORDER BY g.updatedAt DESC
 """)
   Page<Group> searchWithKeywordOnly(@Param("keyword") String keyword, Pageable pageable);
 
   @EntityGraph(attributePaths = {"groupTags", "groupTags.tag"})
+  @Query("""
+    SELECT DISTINCT g FROM Group g
+    LEFT JOIN g.groupTags gt
+    LEFT JOIN gt.tag t
+    WHERE g.status <> 'DISBANDED' AND (
+        g.category =:category
+    )
+    ORDER BY g.updatedAt DESC
+""")
   Page<Group> findByCategoryAndStatusNot(String category, GroupStatus status, Pageable pageable);
 
+
   @EntityGraph(attributePaths = {"groupTags", "groupTags.tag"})
+  @Query("""
+    SELECT DISTINCT g FROM Group g
+    LEFT JOIN g.groupTags gt
+    LEFT JOIN gt.tag t
+    WHERE g.status <> 'DISBANDED'
+    ORDER BY g.updatedAt DESC
+""")
   Page<Group> findByStatusNot(GroupStatus status, Pageable pageable);
 
   boolean existsByIdAndOwnerId(Long groupId, Long userId);
