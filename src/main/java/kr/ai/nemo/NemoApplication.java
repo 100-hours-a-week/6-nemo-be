69d32d976b4dd63df77cb7075e7092b2eb871bb2
kdaehun00 <kdaehun00@naver.com>
2025-06-15 23:04:43 +0900

    feat: @EnableAsync 설정

--- src/main/java/kr/ai/nemo/NemoApplication.java
+++ src/main/java/kr/ai/nemo/NemoApplication.java
@@ -2,8 +2,10 @@ package kr.ai.nemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class NemoApplication {
 
 	public static void main(String[] args) {
