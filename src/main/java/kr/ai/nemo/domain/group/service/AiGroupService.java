3b0039b797e089980dbe6d33b6fc10d4d0e0ae18
kdaehun00 <kdaehun00@naver.com>
2025-06-19 17:13:32 +0900

    fix: ai 통신 컬럼 로깅 코드 삭제

--- src/main/java/kr/ai/nemo/domain/group/service/AiGroupService.java
+++ src/main/java/kr/ai/nemo/domain/group/service/AiGroupService.java
@@ -62,7 +62,6 @@ public class AiGroupService {
         request,
         new ParameterizedTypeReference<>() {}
     );
-    System.out.println("1312312312   " + response.groupId() + "    AI가 전달해준 내용");
     Group group = groupValidator.findByIdOrThrow(response.groupId());
     return new GroupAiRecommendResponse(response.groupId(), response.reason(), group);
   }
