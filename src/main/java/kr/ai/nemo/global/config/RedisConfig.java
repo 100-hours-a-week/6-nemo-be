62e243d725c6c1d481ebcd3ed1e49300a4b4963a
kdaehun00 <kdaehun00@naver.com>
2025-06-22 05:44:42 +0900

    feat: redis cache를 위한 redisConfig 추가

--- src/main/java/kr/ai/nemo/global/config/RedisConfig.java
+++ src/main/java/kr/ai/nemo/global/config/RedisConfig.java
@@ -1,12 +1,17 @@
 package kr.ai.nemo.global.config;
 
+import java.time.Duration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
   @Bean
@@ -18,5 +23,16 @@ public class RedisConfig {
     template.setValueSerializer(new StringRedisSerializer());
 
     return template;
+  }
+
+  @Bean
+  public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
+    RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofMinutes(10)) // 기본 캐시 TTL
+        .disableCachingNullValues();
+
+    return RedisCacheManager.builder(factory)
+        .cacheDefaults(config)
+        .build();
   }
 }
