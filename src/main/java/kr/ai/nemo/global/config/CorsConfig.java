9801ee5364d604eff4519988daeb78080e31aad4
kdaehun00 <kdaehun00@naver.com>
2025-07-22 13:48:34 +0900

    feat: cors 변경

--- src/main/java/kr/ai/nemo/global/config/CorsConfig.java
+++ src/main/java/kr/ai/nemo/global/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("https://dev.nemo.ai.kr", "http://localhost:3000", "http://localhost:8000", "https://localhost:3000", "https://onurivit01.store", "https://localhost.dev.nemo.ai.kr"));
+    configuration.setAllowedOrigins(List.of("https://dev.nemo.ai.kr", "http://localhost:3000", "http://localhost:8000", "https://localhost:3000", "https://onurivit01.store", "https://local.dev.nemo.ai.kr"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
